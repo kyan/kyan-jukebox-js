b96dd88b6b4e53077b3b98d12f5a424624e340d2
Duncan Robertson <duncan@kyan.com>
2018-01-29 20:38:13 +0000

    point to common api host

--- frontend/src/constants/mopidy-api.js
+++ frontend/src/constants/mopidy-api.js
@@ -1,5 +1,5 @@
 export default {
-  WS_URL: 'localhost:8080',
+  WS_URL: 'jukebox-api.local:8080',
   PLAYBACK_GET_CURRENT_TRACK: 'mopidy::playback.getCurrentTrack',
   EVENT_TRACK_PLAYBACK_STARTED: 'mopidy::event:trackPlaybackStarted',
   EVENT_PLAYBACK_STATE_CHANGED: 'mopidy::event:playbackStateChanged',
