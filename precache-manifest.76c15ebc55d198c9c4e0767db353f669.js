08d26b795cfb14bc364934c0fce1b1186f868418
Duncan Robertson <duncan@kyan.com>
2019-08-06 12:23:13 +0100

    Updates

---
+++ precache-manifest.76c15ebc55d198c9c4e0767db353f669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097a5cc863bb345b1cf93480e54edaba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ea699b1bd62ca1a0462",
+    "url": "/static/css/main.33d8a228.chunk.css"
+  },
+  {
+    "revision": "1af67387ccd86524dd07",
+    "url": "/static/js/2.afda7d82.chunk.js"
+  },
+  {
+    "revision": "9ea699b1bd62ca1a0462",
+    "url": "/static/js/main.c0f8fe61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6bba9800156285fc29bd95e579231bd5",
+    "url": "/static/media/default-artwork.6bba9800.png"
+  }
+]);
