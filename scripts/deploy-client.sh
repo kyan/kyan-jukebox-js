a27956baeed6a12b1d1f693b58339f8e83949c28
Duncan Robertson <duncan@kyan.com>
2019-11-08 16:55:37 +0000

    Pass client ID env to frontend

--- scripts/deploy-client.sh
+++ scripts/deploy-client.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+source .env
+
 echo Deploying jukebox client
 
 docker-compose run \
   -e NODE_ENV=production \
   -e REACT_APP_WS_URL=jukebox-api-prod.local \
   -e REACT_APP_WS_PORT=8080 \
+  -e REACT_APP_CLIENT_ID=$CLIENT_ID \
   jukebox-client npm run build
 
 cd frontend && npm run deploy
