3d9d7f6db6f94cbbedde9caf20d63404dd522a5c
Duncan Robertson <duncan@kyan.com>
2019-11-14 10:37:43 +0000

    Fix for client ID in production

--- scripts/deploy-client.sh
+++ scripts/deploy-client.sh
@@ -8,7 +8,7 @@ docker-compose run \
   -e NODE_ENV=production \
   -e REACT_APP_WS_URL=jukebox-api-prod.local \
   -e REACT_APP_WS_PORT=8080 \
-  -e REACT_APP_CLIENT_ID=$CLIENT_ID \
+  -e REACT_APP_CLIENT_ID=$CLIENT_ID_PRODUCTION \
   jukebox-client npm run build
 
 cd frontend && npm run deploy
