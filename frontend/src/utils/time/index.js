bdec342fb587da6d3fd3898658543468bf9996db
Duncan Robertson <duncan@kyan.com>
2019-11-21 08:43:47 +0000

    Add track remaining information

--- frontend/src/utils/time/index.js
+++ frontend/src/utils/time/index.js
@@ -1,8 +1,8 @@
 import ProgressTimer from 'media-progress-timer'
 
 export const millisToMinutesAndSeconds = (millis) => {
-  var minutes = Math.floor(millis / 60000)
-  var seconds = ((millis % 60000) / 1000).toFixed(0)
+  let minutes = Math.floor(millis / 60000)
+  let seconds = ((millis % 60000) / 1000).toFixed(0)
   return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
 }
 
