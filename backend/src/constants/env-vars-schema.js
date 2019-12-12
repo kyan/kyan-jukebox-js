1dfa8f060fbabaac83b53f8f0c5d5c1c2dd9f0a1
Michael Hogg <michael@kyan.com>
2019-12-12 09:22:29 +0000

    (env-vars) Create src/constants/env-vars-schema.js

---
+++ backend/src/constants/env-vars-schema.js
@@ -0,0 +1,12 @@
+export default {
+  CLIENT_ID: /.+/,
+  CLIENT_ID_PRODUCTION: /.+/,
+  LOCAL_STORAGE_PATH: /.+/,
+  MONGODB_URL: /^mongodb:\/\/.+/,
+  NOW_PLAYING_URL: /^https?:\/\/.+/,
+  SPOTIFY_ID: /^[0-9a-f]{32}$/,
+  SPOTIFY_NEW_TRACKS_ADDED_LIMIT: /^\d+$/,
+  SPOTIFY_SECRET: /^[0-9a-f]{32}$/,
+  WS_MOPIDY_PORT: /^\d+$/,
+  WS_MOPIDY_URL: /.+/
+}
