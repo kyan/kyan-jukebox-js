79ebda3891fb9febf41c7777a7c470be423bfa82
helenm <helen.mccarthy@kyan.com>
2020-03-12 10:01:39 +0000

    Fix error

--- frontend/src/components/current-track/index.spec.js
+++ frontend/src/components/current-track/index.spec.js
@@ -111,7 +111,7 @@ describe('CurrentTrack', () => {
         />
       )
 
-      expect(wrapper.find('img')).toMatchSnapshot();
+      expect(wrapper.find('img')).toMatchSnapshot()
       expect(wrapper.find('img').prop('alt')).toMatchSnapshot()
     })
   })
