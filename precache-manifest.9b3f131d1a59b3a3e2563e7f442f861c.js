9ef0975ef2a176778f010ad6fe1d4dd1453e2415
helenm <helen.mccarthy@kyan.com>
2020-03-12 11:17:27 +0000

    Updates

---
+++ precache-manifest.9b3f131d1a59b3a3e2563e7f442f861c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2a40a615ab3da68bf838c0579140dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9681995acd56c332309",
+    "url": "/static/css/2.88febf56.chunk.css"
+  },
+  {
+    "revision": "34f6fbbfcc8af849540d",
+    "url": "/static/css/main.3f195ecf.chunk.css"
+  },
+  {
+    "revision": "e9681995acd56c332309",
+    "url": "/static/js/2.2e110323.chunk.js"
+  },
+  {
+    "revision": "0dc6772ce5863b73c2738c6f2c5e9f2a",
+    "url": "/static/js/2.2e110323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34f6fbbfcc8af849540d",
+    "url": "/static/js/main.bf7eaabc.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "4fccf6214999935986cfda2f89b5ecf0",
+    "url": "/static/media/Biotif-Bold.4fccf621.woff"
+  },
+  {
+    "revision": "d8228f0693a618d073df5d29a68f5f43",
+    "url": "/static/media/Biotif-Bold.d8228f06.woff2"
+  },
+  {
+    "revision": "3402c6f75366f02a39dd158e584f1d0c",
+    "url": "/static/media/Biotif-Light.3402c6f7.woff2"
+  },
+  {
+    "revision": "fa547c317410899d6127f3cc1d905a4f",
+    "url": "/static/media/Biotif-Light.fa547c31.woff"
+  },
+  {
+    "revision": "5a87229da1160c462f62d73e3d347bbb",
+    "url": "/static/media/Biotif-LightItalic.5a87229d.woff2"
+  },
+  {
+    "revision": "6fe4c3ff0bb755a70b569ec4d99df41b",
+    "url": "/static/media/Biotif-LightItalic.6fe4c3ff.woff"
+  },
+  {
+    "revision": "0aacbc8e055a6feb724347106f342f76",
+    "url": "/static/media/Biotif-Regular.0aacbc8e.woff"
+  },
+  {
+    "revision": "b25214d7ebbc3110fd831979e79496de",
+    "url": "/static/media/Biotif-Regular.b25214d7.woff2"
+  },
+  {
+    "revision": "63d839bc0720526d16c2be6da99f1be4",
+    "url": "/static/media/Biotif-SemiBold.63d839bc.woff2"
+  },
+  {
+    "revision": "cb84ffa98139597dfff69bf048b51e76",
+    "url": "/static/media/Biotif-SemiBold.cb84ffa9.woff"
+  },
+  {
+    "revision": "f6680a9daf1352c52d0dfba08b9b60a6",
+    "url": "/static/media/default-artwork.f6680a9d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a005697981665e6f22607d0e08e6d59.js"
+  "/precache-manifest.9b3f131d1a59b3a3e2563e7f442f861c.js"
 );
 
 self.addEventListener('message', (event) => {
