8c287d81645041d5d4bd104152e9469fd3461f64
Duncan Robertson <duncan@kyan.com>
2022-01-13 11:45:01 +0000

    Bling up deploy scripts

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "${DIR}"
 cd ../backend
 
 source .env
 
-echo "Running in '/backend' directory"
+echo "Running in ${YELLOW}/backend${NC} directory"
+echo "Deploying Github branch 'master' /dist directory to Pi..."
 
-echo "Deploying Github 'master' /dist directory to Pi..."
-shipit pi deploy
+read -p "Continue to deploy? " -n 1 -r
+echo # (optional) move to a new line
 
-echo "Done!"
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  shipit pi deploy
+
+  echo "✅ ${GREEN}Deployed successfully.${NC}"
+fi
--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -1,28 +1,39 @@
 #!/bin/sh
 
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "${DIR}"
 cd ../frontend
 
 source .env
 
-echo "Running in '/frontend' directory"
+echo "Running in ${YELLOW}/frontend${NC} directory"
 
 export NODE_ENV=production
 export REACT_APP_WS_URL=$WS_URL_PRODUCTION
 export REACT_APP_WS_PORT=$WS_PORT_PRODUCTION
 export REACT_APP_CLIENT_ID=$CLIENT_ID_PRODUCTION
 
+echo
 echo "Setting:"
-echo "  NODE_ENV: $NODE_ENV"
-echo "  REACT_APP_WS_URL: $REACT_APP_WS_URL"
-echo "  REACT_APP_WS_PORT: $REACT_APP_WS_PORT"
-echo "  REACT_APP_CLIENT_ID: $REACT_APP_CLIENT_ID"
+echo "  ${YELLOW}NODE_ENV:${NC} ${GREEN}$NODE_ENV${NC}"
+echo "  ${YELLOW}REACT_APP_WS_URL:${NC} ${GREEN}$REACT_APP_WS_URL${NC}"
+echo "  ${YELLOW}REACT_APP_WS_PORT:${NC} ${GREEN}$REACT_APP_WS_PORT${NC}"
+echo "  ${YELLOW}REACT_APP_CLIENT_ID:${NC} ${GREEN}$REACT_APP_CLIENT_ID${NC}"
+echo
 
-echo "Building..."
-yarn run build
+read -p "Continue to deploy? " -n 1 -r
+echo # (optional) move to a new line
 
-echo "Deploying to Github..."
-yarn run deploy
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  yarn run build
 
-echo "Done!"
+  echo "Deploying to Github..."
+  yarn run deploy
+
+  echo "✅ ${GREEN}Deployed successfully.${NC}"
+fi
