23e115a3a5a09dab46917f779de7d05976da54ad
dquilter <david@kyan.com>
2022-05-03 14:01:37 +0100

    Adds Sass and Applies to Index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './index.css'
+import './styles/index.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
