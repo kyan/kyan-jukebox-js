0325fde815921856d65a5e07e005785ccc7c4c09
dquilter <david@kyan.com>
2022-05-10 16:42:41 +0100

    Remove Semantic-UI Grid

--- frontend/src/dashboard/index.js
+++ frontend/src/dashboard/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dimmer, Divider, Grid, Header, Container } from 'semantic-ui-react'
+import { Dimmer, Divider, Header } from 'semantic-ui-react'
 import Controls from 'components/controls'
 import DragInTrack from 'components/drag-in-track'
 import CurrentTrackContainer from 'containers/current-track-container'
@@ -9,7 +9,7 @@ import SearchButton from 'search/components/button'
 import VolumeButtons from 'components/volume-buttons'
 import ClearPlaylist from 'components/clear-playlist'
 import SearchContainer from 'search'
-import './index.css'
+import './index.scss'
 
 const Dashboard = props => {
   const {
@@ -34,7 +34,7 @@ const Dashboard = props => {
     <Dimmer.Dimmable blurring dimmed={!online}>
       <DragInTrack disabled={disabled} onDrop={onDrop}>
         <SearchContainer>
-          <Container className='header-wrapper' fluid>
+          <div className='header-wrapper'>
             <Settings />
             <VolumeButtons disabled={disabled} onVolumeChange={onVolumeChange} />
             <SearchButton onClick={onSearchClick} disabled={disabled} />
@@ -46,28 +46,26 @@ const Dashboard = props => {
               onNext={onNext}
               onPrevious={onPrevious}
             />
-          </Container>
+          </div>
           <Divider />
-          <Container className='body-wrapper' fluid>
-            <Grid stackable columns={2} className='dashboard-grid'>
-              <Grid.Column width={4}>
-                <Header size='small'>Current Track</Header>
-                <CurrentTrackContainer />
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <Header size='small'>
-                  Playlist <ClearPlaylist disabled={disabled} onClear={onTracklistClear} />
-                </Header>
-                <TrackList
-                  disabled={disabled}
-                  tracks={tracklist}
-                  currentTrack={currentTrack}
-                  onRemoveTrack={onRemoveTrack}
-                  onArtistSearch={onArtistSearch}
-                />
-              </Grid.Column>
-            </Grid>
-          </Container>
+          <div className='jukeboxWrapper'>
+            <aside>
+              <Header size='small'>Current Track</Header>
+              <CurrentTrackContainer />
+            </aside>
+            <section>
+              <Header size='small'>
+                Playlist <ClearPlaylist disabled={disabled} onClear={onTracklistClear} />
+              </Header>
+              <TrackList
+                disabled={disabled}
+                tracks={tracklist}
+                currentTrack={currentTrack}
+                onRemoveTrack={onRemoveTrack}
+                onArtistSearch={onArtistSearch}
+              />
+            </section>
+          </div>
         </SearchContainer>
       </DragInTrack>
     </Dimmer.Dimmable>
